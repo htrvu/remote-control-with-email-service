20992827cdd85b8df3a4ffa20643be24512c4183
dotrann1412 <dotrann.1412@gmail.com>
2022-04-02 16:07:02 +0700

    delete build content email

--- src/Email.py
+++ src/Email.py
@@ -85,14 +85,6 @@ class Email:
             return False
         return True
 
-def build_email_content(mail_from, mail_to, header, body):
-    email_message = email.message.EmailMessage()
-    email_message.add_header('To', ', '.join(mail_to))
-    email_message.add_header('From', mail_from)
-    email_message.add_header('Subject', header)
-    email_message.add_header('X-Priority', '1')  # Urgency, 1 highest, 5 lowest
-    email_message.set_content(body)
-    return email_message
 
 if __name__ == '__main__':
     EMAIL = "bot.remote.1@gmail.com"
