410ffe1832c38e67df763c8cc62f4a555469834b
dotrann1412 <dotrann.1412@gmail.com>
2022-04-02 15:52:58 +0700

    update utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,15 @@
 import base64
+import email.message
 
 def base64_decode(str):
     str = base64.b64decode(str)
     return str.decode('utf-8')
+
+def build_email_content(mail_from, mail_to, header, body):
+    email_message = email.message.EmailMessage()
+    email_message.add_header('To', ', '.join(mail_to))
+    email_message.add_header('From', mail_from)
+    email_message.add_header('Subject', header)
+    email_message.add_header('X-Priority', '1')  # Urgency, 1 highest, 5 lowest
+    email_message.set_content(body)
+    return email_message
