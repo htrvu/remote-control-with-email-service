891ab841c31345279a18f2b7c04caf910d11745d
dotrann1412 <dotrann.1412@gmail.com>
2022-04-23 10:43:38 +0700

    add entry point

---
+++ src/run.py
@@ -0,0 +1,33 @@
+'''
+    split thread
+'''
+
+
+from utils import *
+
+def show_notification_thead(timout = 300):
+    pass
+
+def check_email_thread(timout = 30):
+    pass
+
+def check_missing_thread(timeout = 300):
+    '''
+        @todo
+        - check the internet connection
+        - check config file
+        - check checkpoint file
+    '''
+    pass
+
+def main_process():
+    pass
+
+def setup():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
