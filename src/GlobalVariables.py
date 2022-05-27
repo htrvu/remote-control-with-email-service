044df4782daa9633f249d56f5174dce05f633448
mathesics <gaxe1102@gmail.com>
2022-05-27 08:38:23 +0700

    add path to all shots

--- src/GlobalVariables.py
+++ src/GlobalVariables.py
@@ -4,4 +4,4 @@ app_location = os.getcwd()
 checkpoint_file_name = 'checkpoint.yaml'
 checkpoint_file_path = os.path.join(app_location, 'configs', checkpoint_file_name)
 
-path_to_shots = 'shots/'
+path_to_shots = '../shots/'
