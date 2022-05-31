1d26d45afee4e58d259c0187449f61cc336034b0
H.T.Vu <trongvulqd@gmail.com>
2022-05-31 23:04:54 +0700

    change registry cmd format

--- src/services/help.py
+++ src/services/help.py
@@ -38,10 +38,10 @@ def __help_df():
             'PC restart': 'Restart the device.',
         },
         'REGISTRY': {
-            'REGISTRY get <hive> <key> <subkey>': 'Get the value of the registry <hive>/<key>/<subkey>.',
-            'REGISTRY add <hive> <key> <subkey>': 'Create new <key>/<subkey> to the registry <hive>.',
-            'REGISTRY modify <hive> <key> <subkey> <value> <dtype>': 'Set the new value of registry <hive>/<key>/<subkey> as dtype. The value of dtype must be REG_BINARY, REG_DWORD or REG_QWORD.',
-            'REGISTRY remove <hive> <key> <subkey>': 'Delete the value of registry <hive>/<key>/<subkey>.' 
+            'REGISTRY get <hive>/<key>/<subkey>': 'Get the value of the registry <hive>/<key>/<subkey>.',
+            'REGISTRY add <hive>/<key>/<subkey> <value> <dtype>': 'Create new <key>/<subkey> to the registry <hive>.',
+            'REGISTRY modify <hive>/<key>/<subkey> <value> <dtype>': 'Set the new value of registry <hive>/<key>/<subkey> as dtype. The value of dtype must be REG_BINARY, REG_DWORD or REG_QWORD.',
+            'REGISTRY remove <hive>/<key>/<subkey>': 'Delete the value of registry <hive>/<key>/<subkey>.' 
         }
     }
 
