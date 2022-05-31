22fa99c9a0edf2766d5307ee38ec75f6c76ac210
dotrann1412 <dotrann.1412@gmail.com>
2022-05-31 22:40:52 +0700

    update response content

--- src/services/registry.py
+++ src/services/registry.py
@@ -1,5 +1,5 @@
 import winreg, os, sys
-# from html_generator import html_table, html_msg
+from .html_generator import html_table, html_msg
 
 def __get(hive, key, subkey):
     value, _ = None, None
@@ -8,29 +8,34 @@ def __get(hive, key, subkey):
         value, _ = winreg.QueryValueEx(kp, subkey) # (value, value type)
         winreg.CloseKey(kp)
     except:
-        return False, f'Cannot get value of {hive} {key} {subkey}'
+        return False, f'Failed'
     
-    return True, f"Extract value of {hive} {key} {subkey} successfully!", value
+    return True, f"successfully!", value
 
 def get(hive, key, subkey):
-    status, msg, value = __get(hive, key, subkey)
+    status, msg = __get(hive, key, subkey)
+    if status is None:
+        bold_all = False
+    else:
+        bold_all = True
     response = {
-        'html': None, #html_msg(msg, status),
-        'data': value
+        'html': html_msg(msg, status, bold_all),
+        'data': None
     }
     return response
 
-def __new_key(hive, key, subkey):
+def __new_key(hive, key, subkey, value, dtype):
     try:
         winreg.CreateKey(getattr(winreg, hive), key + r"\\" + subkey)
+        __modify_key(hive, key, subkey, value, dtype)
     except:
         return False, f'Cannot create {hive} {key}'
     return True, f'{hive} {key} created'
 
-def new_key(hive, key, subkey):
-    status, msg = __new_key(hive, key, subkey)
+def new_key(hive, key, subkey, value, dtype):
+    status, msg = __new_key(hive, key, subkey, value, dtype)
     response = {
-        'html': None, #html_msg(msg, status),
+        'html': html_msg(msg, status, True),
         'data': None
     }
     return response
@@ -65,13 +70,13 @@ def __drop_value(hive, key, subkey):
         winreg.DeleteValue(opened_key, subkey)
         winreg.CloseKey(opened_key)
     except:
-        return False, f""
-    return True, f""
+        return False, f"Failed"
+    return True, f"Successfull"
 
 def drop_value(hive, key, subkey):
     status, message = __drop_value(hive, key, subkey)
     response  = {
-        'html': None,
+        'html': html_msg(message, status, True),
         'data': None
     }
     return response
@@ -79,7 +84,7 @@ def drop_value(hive, key, subkey):
 def modify_key(hive, key, subkey, value, dtype):
     status, msg = __modify_key(hive, key, subkey, value, dtype)
     response = {
-        'html': None, #html_msg(msg, status),
+        'html': html_msg(msg, status, True),
         'data': None
     }
     return response
@@ -89,15 +94,15 @@ def __drop_key(hive, key, subkey):
     try:
         winreg.DeleteKey(getattr(winreg, hive), key + r'\\' + subkey)
     except:
-        return False, f"Failed to drop {hive} {key} {value}"
+        return False, f"Failed"
     
-    return True, f"{hive} {key} {value} was deleted."
+    return True, f"Successful"
         
 
 def drop_key(hive, key):
     status, msg = __drop_key(hive, key)
     response = {
-        'html': None, #html_msg(msg, status),
+        'html': html_msg(msg, status, True),
         'data': None
     }
     return response
