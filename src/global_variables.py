9c574891f1dff792cf8fa2832dfdafa9313f4d22
H.T.Vu <trongvulqd@gmail.com>
2022-06-03 00:15:27 +0700

    remove checkpoint

---
+++ src/global_variables.py
@@ -0,0 +1,14 @@
+import os
+
+app_location = os.getcwd()
+
+configs_file_name = 'app_configs.yaml'
+configs_file_path = os.path.join(app_location, 'configs', configs_file_name)
+
+screen_path = os.path.join(os.path.dirname(app_location), 'screen')
+webcam_path = os.path.join(os.path.dirname(app_location), 'webcam')
+
+app_configs = {
+    'white_list': {},
+    'auto_run': False
+}
