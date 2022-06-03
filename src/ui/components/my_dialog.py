46b411da84c8e9ecbe9fe5e696c6b2ed9bb25439
H.T.Vu <trongvulqd@gmail.com>
2022-06-03 15:03:09 +0700

    change font size

--- src/ui/components/my_dialog.py
+++ src/ui/components/my_dialog.py
@@ -13,7 +13,7 @@ class MyDialog(QDialog):
         self.setWindowIcon(QtGui.QIcon('ui/assets/icons/remote_logo.png'))
 
         # Stylesheet
-        self.setStyleSheet("QDialog {background-color: qlineargradient( x1:0 y1:0, x2:1 y2:0, stop:0 #C9D6FF,  stop:1 #E2E2E2); } QLabel {color: #2d1299; font-size: 20px; font-weight: bold;}")
+        self.setStyleSheet("QDialog {background-color: qlineargradient( x1:0 y1:0, x2:1 y2:0, stop:0 #C9D6FF,  stop:1 #E2E2E2); } QLabel {color: #2d1299; font-size: 18px; font-weight: bold;}")
 
         self.setWindowFlag(QtCore.Qt.WindowCloseButtonHint, False)
         self.setWindowFlag(QtCore.Qt.WindowContextHelpButtonHint, False)
