bddd2ba0784397f6eac26925d3961cdcf0affff3
H.T.Vu <trongvulqd@gmail.com>
2022-06-04 17:37:31 +0700

    remove create folder step (not necessary)

--- src/global_variables.py
+++ src/global_variables.py
@@ -5,9 +5,6 @@ app_location = os.getcwd()
 configs_file_name = 'app_configs.yaml'
 configs_file_path = os.path.join(app_location, 'configs', configs_file_name)
 
-# screen_path = os.path.join(os.path.dirname(app_location), 'screen')
-# webcam_path = os.path.join(os.path.dirname(app_location), 'webcam')
-
 app_configs = {
     'white_list': {},
     'auto_run': False
